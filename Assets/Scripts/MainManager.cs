93002f8fc7d68f3473f558a844f91f453d166a08
michaelhodgkins <mhodgkins1997@gmail.com>
2023-04-07 21:58:57 +0100

    Tutorial Complete
    Another practice at data persistence following a tutorial

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -3,74 +3,162 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
+using TMPro;
 
 public class MainManager : MonoBehaviour
 {
-    public Brick BrickPrefab;
-    public int LineCount = 6;
-    public Rigidbody Ball;
+        public Brick BrickPrefab;
+        public int LineCount = 6;
+        public Rigidbody Ball;
 
-    public Text ScoreText;
-    public GameObject GameOverText;
-    
-    private bool m_Started = false;
-    private int m_Points;
-    
-    private bool m_GameOver = false;
+        public Text ScoreText;
 
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
-        for (int i = 0; i < LineCount; ++i)
+        //Fields for display the player info
+        public Text CurrentPlayerName;
+        public Text BestPlayerNameAndScore;
+
+        public GameObject GameOverText;
+
+        private bool m_Started = false;
+        private int m_Points;
+
+        private bool m_GameOver = false;
+
+        //Static variables for holding the best player data
+        private static int BestScore;
+        private static string BestPlayer;
+
+
+        private void Awake()
         {
-            for (int x = 0; x < perLine; ++x)
+            LoadGameRank();
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+            const float step = 0.6f;
+            int perLine = Mathf.FloorToInt(4.0f / step);
+
+            int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
+            for (int i = 0; i < LineCount; ++i)
             {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
-                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
-                brick.PointValue = pointCountArray[i];
-                brick.onDestroyed.AddListener(AddPoint);
+                for (int x = 0; x < perLine; ++x)
+                {
+                    Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
+                    var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
+                    brick.PointValue = pointCountArray[i];
+                    brick.onDestroyed.AddListener(AddPoint);
+                }
             }
+
+            CurrentPlayerName.text = PlayerDataHandle.Instance.PlayerName;
+
+            SetBestPlayer();
+        }
+
+        private void Update()
+        {
+            if (!m_Started)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    m_Started = true;
+                    float randomDirection = Random.Range(-1.0f, 1.0f);
+                    Vector3 forceDir = new Vector3(randomDirection, 1, 0);
+                    forceDir.Normalize();
+
+                    Ball.transform.SetParent(null);
+                    Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
+                }
+            }
+            else if (m_GameOver)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                }
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+        }
+
+        void AddPoint(int point)
+        {
+            m_Points += point;
+            PlayerDataHandle.Instance.Score = m_Points;
+            ScoreText.text = $"Score : {m_Points}";
+        }
+
+        public void GameOver()
+        {
+            m_GameOver = true;
+            CheckBestPlayer();
+            GameOverText.SetActive(true);
+        }
+
+        private void CheckBestPlayer()
+        {
+            int CurrentScore = PlayerDataHandle.Instance.Score;
+
+            if (CurrentScore > BestScore)
+            {
+                BestPlayer = PlayerDataHandle.Instance.PlayerName;
+                BestScore = CurrentScore;
+
+                BestPlayerNameAndScore.text = $"Best Score - {BestPlayer}: {BestScore}";
+
+                SaveGameRank(BestPlayer, BestScore);
+            }
+        }
+
+        private void SetBestPlayer()
+        {
+            if (BestPlayer == null && BestScore == 0)
+            {
+                BestPlayerNameAndScore.text = "";
+            }
+            else
+            {
+                BestPlayerNameAndScore.text = $"Best Score - {BestPlayer}: {BestScore}";
+            }
+
+        }
+
+        public void SaveGameRank(string bestPlayerName, int bestPlayerScore)
+        {
+            SaveData data = new SaveData();
+
+            data.TheBestPlayer = bestPlayerName;
+            data.HighiestScore = bestPlayerScore;
+
+            string json = JsonUtility.ToJson(data);
+            File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+        }
+
+        public void LoadGameRank()
+        {
+            string path = Application.persistentDataPath + "/savefile.json";
+
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
+                SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+                BestPlayer = data.TheBestPlayer;
+                BestScore = data.HighiestScore;
+            }
+        }
+
+        [System.Serializable]
+        class SaveData
+        {
+            public int HighiestScore;
+            public string TheBestPlayer;
         }
     }
 
-    private void Update()
-    {
-        if (!m_Started)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                m_Started = true;
-                float randomDirection = Random.Range(-1.0f, 1.0f);
-                Vector3 forceDir = new Vector3(randomDirection, 1, 0);
-                forceDir.Normalize();
 
-                Ball.transform.SetParent(null);
-                Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
-            }
-        }
-        else if (m_GameOver)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            }
-        }
-    }
-
-    void AddPoint(int point)
-    {
-        m_Points += point;
-        ScoreText.text = $"Score : {m_Points}";
-    }
-
-    public void GameOver()
-    {
-        m_GameOver = true;
-        GameOverText.SetActive(true);
-    }
-}
